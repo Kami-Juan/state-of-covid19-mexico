0e19c9ca27a757229a895d6a6c3ce1052c6153f9
Kami-Juan <juan1910@live.com.mx>
2020-05-08 21:09:34 -0500

    feat: added console messages

--- packages/scrapper/index.js
+++ packages/scrapper/index.js
@@ -10,6 +10,7 @@ const {jsonConverter} = require('./utils');
 
 (async () => {
   try {
+    console.log("Descargando información, espere...");
     await downloader();
 
     const files = await readdir(path.resolve(__dirname, `./${process.env.TMP_FOLDER}`));
@@ -17,8 +18,13 @@ const {jsonConverter} = require('./utils');
     const catalogsFilename = files.find(filename => filename.toLowerCase().includes('catalogos'))
     const covidDataFilename = files.find(filename => filename.toLowerCase().includes('covid19'))
 
+    console.log(`Descargado ${catalogsFilename}`)
+    console.log(`Descargado ${covidDataFilename}`)
+
     const streamCatalogsFile = createReadStream(path.join(__dirname, `./${process.env.TMP_FOLDER}/${catalogsFilename}`));
     const streamCovidDataFile = createReadStream(path.join(__dirname, `./${process.env.TMP_FOLDER}/${covidDataFilename}`));
+
+    console.log("Procesando información...");
 
     const hashTableCatalogs = await processCatalogs(streamCatalogsFile);
     const covidDataJsonStream = await processCovidDataRaw(streamCovidDataFile, hashTableCatalogs);
@@ -27,7 +33,9 @@ const {jsonConverter} = require('./utils');
     await ensureDir(path.join(__dirname, `./${process.env.DATA_FOLDER}`));
     jsonConverter(formattedCovidData, `./${process.env.DATA_FOLDER}/covid-data.json`);
 
+    console.log("Removiendo datos temporales...");
     await remove(path.join(__dirname, `./${process.env.TMP_FOLDER}`));
+    console.log("Terminado");
   } catch (e) {
     console.log(e)
   }
--- packages/scrapper/utils/index.js
+++ packages/scrapper/utils/index.js
@@ -1,24 +1,8 @@
-const { mkdirSync, existsSync, writeFile, ensureDirSync} = require('fs-extra');
-const {createWriteStream} = require('fs')
+const { mkdirSync, existsSync, writeFile, createWriteStream} = require('fs-extra');
 const path = require('path');
 
 const XLSX = require('xlsx');
 const unzipper = require('unzipper');
-
-exports.unzip = (zipStream, filename = null, outputDir = path.join(__dirname, './../data')) => {
-  const zipRequest = zipStream.pipe(unzipper.Parse());
-
-  if (!existsSync(outputDir)) {
-    mkdirSync(`${outputDir}`, {recursive: true});
-  }
-
-  zipRequest.on('entry', (entry) => {
-    const fullZipName = entry.path;
-    const singleName = filename || fullZipName.split("/").pop();
-
-    entry.pipe(createWriteStream(`${outputDir}/${singleName}`));
-  });
-}
 
 exports.promiseUnzip = (zipStream) => {
   return new Promise((resolve, reject) => {
@@ -31,7 +15,6 @@ exports.promiseUnzip = (zipStream) => {
     .on('error', () => reject())
     .pipe(unzipper.Parse())
     .on('entry', (entry) => {
-      // storeStream(entry, entry.path.split("/").pop())
       entry.pipe(createWriteStream(path.join(__dirname, `./../${process.env.TMP_FOLDER}/${entry.path.split("/").pop()}`)))
     });
   })
@@ -59,11 +42,6 @@ exports.readExcelStream = (stream, cb) => {
 exports.convertArrayToMap = (list, key = 'CLAVE') => list.reduce((prev, current) => prev.set(current[key], current), new Map())
 
 exports.getDescriptionByCatalog = (catalog, pos) => catalog.get(parseInt(pos))["DESCRIPCIÓN"].trim()
-
-const storeStream = async (stream, filename) => {
-  // ensureDirSync(`./../${process.env.TMP_FOLDER}`);
-  stream.pipe(createWriteStream(path.resolve(__dirname, `./../${process.env.TMP_FOLDER}/${filename}`)))
-}
 
 exports.jsonConverter = (data, dir) => {
   const json = JSON.stringify(data, null, 2);
